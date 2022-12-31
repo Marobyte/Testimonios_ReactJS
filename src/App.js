12f12c58d7b963d282b01f2726cbfb57af033099
Maro Martinez R <maromartinezrivas@gmail.com>
2022-12-31 15:35:12 -0300

    Visulizacion de Testimonios en ReactJS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/testimonios'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contendor-principal'>
+      <h1>Testimonios de Nuestra Universidad.</h1>
+      </div>
+      <Testimonio 
+      nombre='Maro Martinez'
+      pais='Chile'
+      imagen='maro'
+      cargo='Ingeniero de Software'
+      empresa='Amazon'
+      testimonio='Laborum ullamco fugiat dolore aliqua enim nostrud voluptate laborum deserunt irure. Tempor cupidatat Lorem in officia ex aute velit commodo cillum aute veniam. Eu et ut aliquip est. Nostrud non duis ipsum laboris esse. Exercitation duis sit consequat laborum do aute quis velit labore Lorem ipsum consectetur aliquip.' />
+      <Testimonio 
+      nombre='Raul Bostian'
+      pais='Suecia'
+      imagen='raul'
+      cargo='Ingeniero de Software'
+      empresa='Spotify'
+      testimonio='Laborum ullamco fugiat dolore aliqua enim nostrud voluptate laborum deserunt irure. Tempor cupidatat Lorem in officia ex aute velit commodo cillum aute veniam. Eu et ut aliquip est. Nostrud non duis ipsum laboris esse. Exercitation duis sit consequat laborum do aute quis velit labore Lorem ipsum consectetur aliquip.' />
+      
+    <Testimonio 
+      nombre='Emma Rosas'
+      pais='Venezuela'
+      imagen='emma'
+      cargo='Ingeniero de Software'
+      empresa='NTTData'
+      testimonio='Laborum ullamco fugiat dolore aliqua enim nostrud voluptate laborum deserunt irure. Tempor cupidatat Lorem in officia ex aute velit commodo cillum aute veniam. Eu et ut aliquip est. Nostrud non duis ipsum laboris esse. Exercitation duis sit consequat laborum do aute quis velit labore Lorem ipsum consectetur aliquip.'  />
     </div>
   );
 }
